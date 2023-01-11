5877d07b4b9b7f05a90f73debac8fc7c1608d6b2
diego-mac <dijavaji@gmail.com>
2023-01-11 11:09:55 -0500

    primer commit

---
+++ tec-car-manager-ws/src/main/java/ec/com/technoloqie/car/manager/apirest/ws/TecCarManagerWsApplication.java
@@ -0,0 +1,13 @@
+package ec.com.technoloqie.car.manager.apirest.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecCarManagerWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecCarManagerWsApplication.class, args);
+	}
+
+}
---
+++ tec-car-manager-ws/src/test/java/ec/com/technoloqie/car/manager/apirest/ws/TecCarManagerWsApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.technoloqie.car.manager.apirest.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecCarManagerWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
